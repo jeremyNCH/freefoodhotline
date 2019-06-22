9da5eab355cbab3d364f320247dea8b78607cd77
Kyle Meade <kyle.meade37@gmail.com>
2019-06-22 17:18:19 -0400

    adding profile routes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(express.json({
 // Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/profile', require('./routes/profile'));
 
 const PORT = process.env.PORT || 5000;
 
