58a1b28d6f49ddbd446e5b6524a72c1139a99b26
jeremyNCH <jnchafok@uwaterloo.ca>
2019-06-22 21:35:02 -0400

    finishing twilio starting azure

--- twilio.js
+++ twilio.js
@@ -1,18 +1,65 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const r = require('request');
+const https = require('https');
+const fs = require('fs');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
-http
-  .createServer((req, res) => {
-    // Create TwiML response
-    const twiml = new VoiceResponse();
+const accountSID = 'AC95892fb3be32f636805a227c5e85d7ef';
+const authToken = '5425f58382ec0fea69a20ad4e3815052';
+const client = require('twilio')(accountSID, authToken);
 
-    twiml.say('Hello from your pals at Twilio! Have fun.');
+const app = express();
+app.use(bodyParser.json({
+  extended: true
+}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-    res.writeHead(200, {
-      'Content-Type': 'text/xml'
+// Returns TwiML which prompts the caller to record a message
+app.post('/', async (req, res) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  const twiml = new VoiceResponse();
+  twiml.say('Hello. Please leave a message after the beep.');
+
+  // Use <Record> to record the caller's message
+  twiml.record({
+    recordingStatusCallback: '/callAzure'
+  });
+
+  // End the call with <Hangup>
+  twiml.hangup();
+
+  // Render the response as XML in reply to the webhook request
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+app.post('/callAzure', (req, res) => {
+  const recordingURL = req.body.RecordingUrl;
+  console.log('recordingURL: ', recordingURL);
+
+  let file = fs.createWriteStream("record.wav");
+  try {
+    // r.get(recordingURL + '.mp3')
+    //   .on('response', res => {
+    //     if (res.statusCode === 200) {
+    console.log('getting record...');
+    //       req.pipe(file);
+    //     }
+    //   });
+    var request = https.get(recordingURL, function (response) {
+      response.pipe(file);
     });
-    res.end(twiml.toString());
-  })
-  .listen(1337, '127.0.0.1');
+  } catch (err) {
+    console.log('get record failed...', err);
+    throw err;
+  }
 
-console.log('TwiML server running at http://127.0.0.1:1337/');
+  res.send('calling Azure...');
+});
+
+// Create an HTTP server and listen for requests on port 3000
+console.log('Twilio running on port 1337');
+app.listen(1337, '127.0.0.1');
